a5e4a99f0c3bc9b05467c59350e0e669d803b219
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-04-30 00:33:45 +0900

    init commit

---
+++ src/test/java/sample/ox_for_junior/OxForJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.ox_for_junior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxForJuniorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
